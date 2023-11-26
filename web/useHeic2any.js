2152d5f2f639572d2e2688ad3189d8d7de4d85d4
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-11-26 01:28:54 +0100

    Add example project.

--- web/useHeic2any.js
+++
@@ -1,9 +0,0 @@
-function executeHeic2any(blob, multiple, toType, quality, gifInterval) {
-	return heic2any({
-		blob,
-		multiple: multiple,
-		toType: toType,
-		quality: quality,
-		gifInterval: gifInterval
-	});
-}
